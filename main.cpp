5550c687a42448d8c78f00bd38230d759683437f
Hafiz Muhammad Luqman <hafizlouqman@gmail.com>
2017-06-23 02:44:41 +0200

    Update: tried to calculate rectification with different parameters

--- main.cpp
+++ main.cpp
@@ -6,46 +6,98 @@
 #include <cmath>
 #include<iostream>
 
+//Autonomous Data
 using namespace cv;
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
 
     Mat leftImage, rightImage;
     leftImage = imread("/lhome/luqman/Work/stereo_tutorial/images/leftImg.png");
     rightImage = imread("/lhome/luqman/Work/stereo_tutorial/images/rightImg.png");
     Mat leftGrayImg, rightGrayImg;
+//    std::cout <<"The type of input images "<<leftImage.type()<<std::endl;
 
-    cvtColor(leftImage,leftGrayImg,COLOR_BGR2GRAY);
-    cvtColor(rightImage,rightGrayImg,COLOR_BGR2GRAY);
-    imshow("point_cloud_filename.png", leftGrayImg);
-    cvWaitKey();
+//    cvtColor(leftImage,leftGrayImg,COLOR_BGR2GRAY);
+//    cvtColor(rightImage,rightGrayImg,COLOR_BGR2GRAY);
+//    imshow("point_cloud_filename.png", leftGrayImg);
+//    cvWaitKey();
 
-    std::cout <<"The type of input images "<<leftGrayImg.type()<<std::endl;
+//    std::cout <<"The type of input images "<<leftGrayImg.type()<<std::endl;
     // Left camera intrinsics
     cv::Mat intrinLeft = cv::Mat(3,3,CV_64F);
-    intrinLeft.at<double>(0,0) = 721.54;
+    intrinLeft.at<double>(0,0) = 643.6696894899887;
     intrinLeft.at<double>(0,1) = 0.0;
-    intrinLeft.at<double>(0,2) = 609.56;
+    intrinLeft.at<double>(0,2) = 362.55693461986806;
     intrinLeft.at<double>(1,0) = 0;
-    intrinLeft.at<double>(1,1) = 721.54;
-    intrinLeft.at<double>(1,2) = 172.85;
+    intrinLeft.at<double>(1,1) = 643.195958095438;
+    intrinLeft.at<double>(1,2) = 235.97439375017078;
     intrinLeft.at<double>(2,0) = 0.0;
     intrinLeft.at<double>(2,1) = 0.0;
     intrinLeft.at<double>(2,2) = 1.0;
 
     // Right camera intrinsics
     cv::Mat intrinRight = cv::Mat(3,3,CV_64F);
-    intrinRight.at<double>(0,0) = 721.54;
+    intrinRight.at<double>(0,0) = 646.5493349750983;
     intrinRight.at<double>(0,1) = 0;
-    intrinRight.at<double>(0,2) = 609.56;
+    intrinRight.at<double>(0,2) = 371.2506584573806;
     intrinRight.at<double>(1,0) = 0;
-    intrinRight.at<double>(1,1) = 721.54;
-    intrinRight.at<double>(1,2) = 172.85;
+    intrinRight.at<double>(1,1) = 646.114994072424;
+    intrinRight.at<double>(1,2) = 240.7298493693128;
     intrinRight.at<double>(2,0) = 0;
     intrinRight.at<double>(2,1) = 0;
     intrinRight.at<double>(2,2) = 1;
+
+    //Left camera distortions parameters
+    cv::Mat distorLeft = cv::Mat(5,1,CV_64F);
+    distorLeft.at<double>(0,0) = -0.40857872522130634;
+    distorLeft.at<double>(1,0) = 0.17153250043015486;
+    distorLeft.at<double>(2,0) = -0.00017086167957252753;
+    distorLeft.at<double>(3,0) = -0.0001514669827181509;
+    distorLeft.at<double>(4,0) = 0.0;
+
+    //Right camera distortions parameters
+    cv::Mat distorRight = cv::Mat(5,1,CV_64F);
+    distorRight.at<double>(0,0) = -0.4059598651487747;
+    distorRight.at<double>(1,0) = 0.17085484976716034;
+    distorRight.at<double>(2,0) = -0.0008339149470988998;
+    distorRight.at<double>(3,0) = 0.0003865393687439495;
+    distorRight.at<double>(4,0) = 0.0;
+
+    double roll = -1.639, pitch = -0.005, yaw = -1.597;
+    cv::Mat rollMat = cv::Mat(3,3,CV_64F);
+    rollMat.at<double>(0,0) = cos(roll);
+    rollMat.at<double>(0,1) = sin(roll);
+    rollMat.at<double>(0,2) = 0;
+    rollMat.at<double>(1,0) = -sin(roll);
+    rollMat.at<double>(1,1) = cos(roll);
+    rollMat.at<double>(1,2) = 0;
+    rollMat.at<double>(2,0) = 0.0;
+    rollMat.at<double>(2,1) = 0.0;
+    rollMat.at<double>(2,2) = 1.0;
+
+    cv::Mat yawMat = cv::Mat(3,3,CV_64F);
+    yawMat.at<double>(0,0) = cos(yaw);
+    yawMat.at<double>(0,1) = 0;
+    yawMat.at<double>(0,2) = -sin(yaw);
+    yawMat.at<double>(1,0) = 0;
+    yawMat.at<double>(1,1) = 1.0;
+    yawMat.at<double>(1,2) = 0;
+    yawMat.at<double>(2,0) = sin(yaw);
+    yawMat.at<double>(2,1) = 0.0;
+    yawMat.at<double>(2,2) = cos(yaw);
+
+    cv::Mat pitchMat = cv::Mat(3,3,CV_64F);
+    pitchMat.at<double>(0,0) = 1.0;
+    pitchMat.at<double>(0,1) = 0.0;
+    pitchMat.at<double>(0,2) = 0.0;
+    pitchMat.at<double>(1,0) = 0.0;
+    pitchMat.at<double>(1,1) = cos(pitch);
+    pitchMat.at<double>(1,2) = sin(pitch);
+    pitchMat.at<double>(2,0) = 0.0;
+    pitchMat.at<double>(2,1) = -sin(pitch);
+    pitchMat.at<double>(2,2) = cos(pitch);
 
     cv::Mat rotMat = cv::Mat(3,3,CV_64F);
     rotMat.at<double>(0,0) = 1.00;
@@ -57,28 +109,48 @@ int main(int argc, char *argv[])
     rotMat.at<double>(2,0) = 0.0;
     rotMat.at<double>(2,1) = 0.0;
     rotMat.at<double>(2,2) = 1.0;
+    //rotMat = rollMat * yawMat * pitchMat;
+
+
 
     cv::Mat translationMat = cv::Mat(3,1,CV_64F);
-    translationMat.at<double>(0,0) = 387.57/721.54;
-    translationMat.at<double>(1,0) = 0.0;
-    translationMat.at<double>(2,0) = 0.0;
+    translationMat.at<double>(0,0) = 153.50167013008075/646.5493349750983; // -1.380//
+    translationMat.at<double>(1,0) = 0.007;
+    translationMat.at<double>(2,0) = 0.002;
 
     cv::Size imgSize = leftImage.size();
 
-    int ndisparities = 16*5;
-    int SADWindowSize = 21;
+    cv::Mat oRotleft, oRotRight, projLeft, projRight, Q;
+//    int flags = 0; double alpha=-1;
+//    cv::Rect roi1, roi2;
 
-    StereoBM bm(StereoBM::BASIC_PRESET, 32, 9);
+    cv::stereoRectify(intrinLeft, distorLeft, intrinRight, distorRight,
+                   imgSize, rotMat, translationMat, oRotleft, oRotRight, projLeft, projRight,
+                   Q, 0, -1,imgSize);
+
+    cv::Mat map11, map12, map21, map22;
+    cv::initUndistortRectifyMap(intrinLeft, distorLeft, oRotleft, projLeft, imgSize, CV_16SC2, map11, map12);
+    cv::initUndistortRectifyMap(intrinRight, distorRight, oRotRight, projRight, imgSize, CV_16SC2, map21, map22);
+
+    cv::Mat img1r, img2r;
+    cv::remap(leftImage, img1r, map11, map12, cv::INTER_LINEAR);
+    cv::remap(rightImage, img2r, map21, map22, cv::INTER_LINEAR);
+
+    imshow("point_cloud_filename.png", img2r);
+    cvWaitKey();
+
+
+    StereoBM bm(StereoBM::BASIC_PRESET, 16, 9);
 
     bm.state->preFilterType = CV_STEREO_BM_XSOBEL;
 
     bm.state->preFilterCap = 63;
 
-    bm.state->SADWindowSize = 11;
+    bm.state->SADWindowSize = 9;
 
     bm.state->minDisparity = 0;
 
-    bm.state->numberOfDisparities = 32;
+    bm.state->numberOfDisparities = 16;
 
     bm.state->textureThreshold = 3;
 
@@ -92,10 +164,10 @@ int main(int argc, char *argv[])
 
     cv::Mat disparityMat;
 
-    bm(leftGrayImg, rightGrayImg, disparityMat);
+//    bm(img1r, img2r, disparityMat);
 
-    imshow("point_cloud_filename.png", disparityMat);
-    cvWaitKey();
+//    imshow("point_cloud_filename.png", disparityMat);
+//    cvWaitKey();
 
 //    Mat xyz;
 //    reprojectImageTo3D(disparityMat, xyz, Q, false, CV_32F);
