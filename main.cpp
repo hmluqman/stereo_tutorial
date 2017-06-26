fe8539a164b2aa6e2b71d25520b5a3675fe9fd11
Hafiz Muhammad Luqman <hafizlouqman@gmail.com>
2017-06-26 17:44:16 +0200

    depth map created

--- main.cpp
+++ main.cpp
@@ -45,6 +45,30 @@ int main(int argc, char *argv[])
     intrinRight.at<double>(2,1) = 0;
     intrinRight.at<double>(2,2) = 1;
 
+    // Left camera calibration matrix
+    cv::Mat cameraMatrix1 = cv::Mat(3,3,CV_64F);
+    cameraMatrix1.at<double>(0,0) = 9.842439e+02;
+    cameraMatrix1.at<double>(0,1) = 0.000000e+00;
+    cameraMatrix1.at<double>(0,2) = 6.900000e+02;
+    cameraMatrix1.at<double>(1,0) = 0.000000e+00;
+    cameraMatrix1.at<double>(1,1) = 9.808141e+02;
+    cameraMatrix1.at<double>(1,2) = 2.331966e+02;
+    cameraMatrix1.at<double>(2,0) = 0.000000e+00;
+    cameraMatrix1.at<double>(2,1) = 0.000000e+00;
+    cameraMatrix1.at<double>(2,2) = 1.000000e+00;
+
+    // Right camera calibration matrix
+    cv::Mat cameraMatrix2 = cv::Mat(3,3,CV_64F);
+    cameraMatrix2.at<double>(0,0) = 9.895267e+02;
+    cameraMatrix2.at<double>(0,1) = 0.000000e+00;
+    cameraMatrix2.at<double>(0,2) = 7.020000e+02;
+    cameraMatrix2.at<double>(1,0) = 0.000000e+00;
+    cameraMatrix2.at<double>(1,1) = 9.878386e+02;
+    cameraMatrix2.at<double>(1,2) = 2.455590e+02;
+    cameraMatrix2.at<double>(2,0) = 0.000000e+00;
+    cameraMatrix2.at<double>(2,1) = 0.000000e+00;
+    cameraMatrix2.at<double>(2,2) = 1.000000e+00;
+
     cv::Mat rotMat = cv::Mat(3,3,CV_64F);
     rotMat.at<double>(0,0) = 1.00;
     rotMat.at<double>(0,1) = 0.0;
@@ -61,11 +85,34 @@ int main(int argc, char *argv[])
     translationMat.at<double>(1,0) = 0.0;
     translationMat.at<double>(2,0) = 0.0;
 
+    //Left camera distortions parameters
+    cv::Mat distorLeft = cv::Mat(5,1,CV_64F);
+    distorLeft.at<double>(0,0) = -0.37288;
+    distorLeft.at<double>(1,0) = 0.20373;
+    distorLeft.at<double>(2,0) = 0.0022190;
+    distorLeft.at<double>(3,0) = 0.0013837;
+    distorLeft.at<double>(4,0) = -0.072337;
+
+    //Right camera distortions parameters
+    cv::Mat distorRight = cv::Mat(5,1,CV_64F);
+    distorRight.at<double>(0,0) = -0.36447;
+    distorRight.at<double>(1,0) = 0.17900;
+    distorRight.at<double>(2,0) = 0.0011481;
+    distorRight.at<double>(3,0) = -6.298563e-04;
+    distorRight.at<double>(4,0) = -5.314062e-02;
+
+
     cv::Size imgSize = leftImage.size();
+
+    cv::Mat R1, R2, P1, P2, Q;
+    // Calculation of stereo rectify transform
+    cv::stereoRectify(cameraMatrix1, distorLeft, cameraMatrix2, distorRight, imgSize, rotMat,
+                      translationMat, R1, R2, P1, P2, Q);
 
     int ndisparities = 16*5;
     int SADWindowSize = 21;
     cv::Mat disparityMat, disparityMat8;
+
     //CLass for computing stereo correspondence by Block Matching Algorithm
 
     StereoBM bm(StereoBM::BASIC_PRESET, ndisparities, SADWindowSize);
@@ -84,7 +131,10 @@ int main(int argc, char *argv[])
     normalize(disparityMat, disparityMat8, 0, 255, CV_MINMAX, CV_8U);
     imshow("point_cloud_filename.png", disparityMat8);
     cvWaitKey();
-
+    cv::Mat _3dImage;
+    cv::reprojectImageTo3D(disparityMat,_3dImage, Q, true);
+    imshow("Depth map", _3dImage);
+    cvWaitKey();
     //**CLass for computing stereo correspondence by Semi Block Matching Algorithm
 /*
     StereoBM sbm;
